8cae4b77692d6cd90583687e90f0a0ceb040a0ed
av3656 <communicate.aman@yahoo.com>
2026-04-17 03:41:08 +0530

    Initial commit - A Journal Application using Spring Boot
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/vermatradingcompany/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,78 @@
+package com.vermatradingcompany.journalApp.controller;
+
+import com.vermatradingcompany.journalApp.entity.JournalEntry;
+import com.vermatradingcompany.journalApp.entity.User;
+import com.vermatradingcompany.journalApp.repository.JournalEntryRepository;
+import com.vermatradingcompany.journalApp.service.JournalEntryService;
+import com.vermatradingcompany.journalApp.service.UserService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{userName}")
+    public ResponseEntity<?> getAllJournalEntriesOfUser(@PathVariable String userName) {
+        User user = userService.findByUserName(userName);
+        List<JournalEntry> all = user.getJournalEntries();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("{userName}")
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName) {
+        try {
+            journalEntryService.saveEntry(myEntry, userName);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(myId);
+        if (journalEntry.isPresent()) {
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("id/{userName}/ {myId}")
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId, @PathVariable String userName) {
+        journalEntryService.deleteById(myId, userName);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("id/{userName}/{myId}")
+    public ResponseEntity<?> updateJournalEntryById(@PathVariable ObjectId myId,
+                                                    @RequestBody JournalEntry newEntry,
+                                                    @PathVariable String userName) {
+        JournalEntry old = journalEntryService.findById(myId).orElse(null);
+        if (old != null) {
+            if (newEntry.getTitle() != null && !newEntry.getTitle().isEmpty()) {
+                old.setTitle(newEntry.getTitle());
+            }
+            if (newEntry.getContent() != null && !newEntry.getContent().isEmpty()) {
+                old.setContent(newEntry.getContent());
+            }
+            journalEntryService.saveEntry(old);
+            return new ResponseEntity<>(old, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/vermatradingcompany/journalApp/service/JournalEntryService.java
@@ -0,0 +1,57 @@
+package com.vermatradingcompany.journalApp.service;
+
+import com.vermatradingcompany.journalApp.entity.JournalEntry;
+import com.vermatradingcompany.journalApp.entity.User;
+import com.vermatradingcompany.journalApp.repository.JournalEntryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+    @Autowired
+    private UserService userService;
+
+    public void saveEntry (JournalEntry journalEntry,String userName){
+        try {
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
+        }
+        catch (Exception e){
+            log.error("Ecception",e);
+        }
+    }
+
+    public void saveEntry (JournalEntry journalEntry){
+       journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id, String userName){
+        User user = userService.findByUserName(userName);
+        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+        userService.saveEntry(user);
+        journalEntryRepository.deleteById(id);
+    }
+
+
+}
